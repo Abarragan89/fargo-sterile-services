cf15de28fed175cd140c5bf75e369075835e9ceb
Anthony Barragan <anthony.bar.89@gmail.com>
2024-12-16 14:27:58 -0800

    first pdf basic strucutre is done. Getting data from the form minus the terms and service

--- src/app/api/sendEmail/route.ts
+++ src/app/api/sendEmail/route.ts
@@ -13,7 +13,7 @@ export async function POST(request: NextRequest) {
 
     const message: MailDataRequired = {
         from: process.env.GOOGLE_USER as string,
-        to: 'anthony.bar.89@gmail.com',
+        to: 'Justin.Feagin@fagronsterile.com',
         subject: 'Math Fact Missions - Activate Account',
         html: `
             <h3>Hello Anthony,</h3>
