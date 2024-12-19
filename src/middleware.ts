20664fd8ceefc6d1908376e289813b13ac01979b
Anthony Barragan <anthony.bar.89@gmail.com>
2024-12-19 11:40:06 -0800

    added 8mb to request

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,6 @@ export function middleware(request: NextRequest) {
         script-src 'self' 'nonce-${nonce}';
         style-src 'self' 'unsafe-inline';
         img-src 'self' blob: data:;
-        img-src 'self' blob: data: http://localhost:3000;
         font-src 'self';
         frame-src 'self' blob:;
         object-src 'none';
@@ -21,18 +20,18 @@ export function middleware(request: NextRequest) {
     `;
     }
     else if (process.env.NODE_ENV === 'development') {
-    //     cspHeader = `
-    //     default-src 'self';
-    //     script-src 'self' 'unsafe-inline' 'unsafe-eval' http://localhost:3000;
-    //     style-src 'self' 'unsafe-inline';
-    //     img-src 'self' blob: data: http://localhost:3000;
-    //     font-src 'self';
-    //     object-src 'none';
-    //     base-uri 'self';
-    //     form-action 'self';
-    //     frame-ancestors 'none';
-    //     upgrade-insecure-requests;
-    // `;
+        //     cspHeader = `
+        //     default-src 'self';
+        //     script-src 'self' 'unsafe-inline' 'unsafe-eval' http://localhost:3000;
+        //     style-src 'self' 'unsafe-inline';
+        //     img-src 'self' blob: data: http://localhost:3000;
+        //     font-src 'self';
+        //     object-src 'none';
+        //     base-uri 'self';
+        //     form-action 'self';
+        //     frame-ancestors 'none';
+        //     upgrade-insecure-requests;
+        // `;
     }
 
     const contentSecurityPolicyHeaderValue = cspHeader.replace(/\s{2,}/g, ' ').trim();
