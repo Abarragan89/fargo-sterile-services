3841c95e73390296a71203f984d2a3c2a177ed1a
Anthony Barragan <anthony.bar.89@gmail.com>
2025-02-16 00:30:51 -0800

    updated message for request entityto large

--- src/app/reviewInformation/page.tsx
+++ src/app/reviewInformation/page.tsx
@@ -126,6 +126,7 @@ export default function ReviewPage() {
                         <>
                             <p className="text-center mx-10 italic mt-3">Your combined uploaded documents are too large. Update your files in</p>
                             <p className="text-center mx-10 italic mt-3 font-bold italic">&apos;Document Uploads&apos;.</p>
+                            <p className="text-center mx-10 italic mt-3">Only include necessary files and pages.</p>
                         </>
                         :
                         <p className="text-center mx-10 italic mt-3">{errorMessage?.message}</p>
